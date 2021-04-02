1b46644b756a985873a8528fe687724ebd6695d6
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-02 12:28:51 -0300

    CSS al Header

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -11,6 +11,7 @@ const Layout = props => {
                     :root {
                         --gris: #3D3D3D;
                         --gris2: #6F6F6F;
+                        --gris3: #E1E1E1;
                         --naranja: #DA552F
                     }
 
