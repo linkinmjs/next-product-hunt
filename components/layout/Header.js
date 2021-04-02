866327f539d357d1306bb4c9e3b5745d4b13c48a
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-02 16:10:30 -0300

    CSS a la Barra

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Buscar from '../ui/Buscar';
-import Navegacion from './Navegacion';
 import Link from 'next/link';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
+import Buscar from '../ui/Buscar';
+import Navegacion from './Navegacion';
+import Boton from '../ui/Boton';
 
 const ContenedorHeader = styled.div`
     max-width: 1200px;
@@ -25,6 +26,9 @@ const Logo = styled.p`
 `;
 
 const Header = () => {
+
+    const usuario = true;
+
     return ( 
         <header
             css={css`
@@ -41,13 +45,38 @@ const Header = () => {
 
                     <Navegacion />
                 </div>
-                <div>
-                    <p>Hola: Stimpy</p>
+                <div
+                    css={css`
+                        display: flex;
+                        align-items: center;
+                    `}
 
-                    <button type="button">Cerrar sesión</button>
+                >
 
-                    <Link href="/">Login</Link>
-                    <Link href="/">Crear Cuenta</Link>
+                { usuario ? (
+                    <>
+                        <p
+                            css={css`
+                                margin-right: 2rem;
+                            `}
+                        >Hola: Stimpy</p>
+                        <Boton 
+                            bgColor="true"
+                        >Cerrar sesión</Boton>
+                    </>
+                ) : (
+                    <>
+                        <Link href="/">
+                            <Boton
+                                bgColor="true"
+                            >Login</Boton>
+                        </Link>
+                        <Link href="/">
+                            <Boton>Crear Cuenta</Boton>
+                        </Link>
+                    </>
+                ) }
+
                 </div>
             </ContenedorHeader>
         </header>
