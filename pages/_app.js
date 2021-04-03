898a2c76eb5f7733f29d50419db93869270cdeb6
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-03 16:00:32 -0300

    Creando el Context de Firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import firebase, { FirebaseContext, firebaseContext } from '../firebase';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  
+
+  return (
+    <FirebaseContext.Provider
+      value={{
+        firebase
+      }}
+    >
+      <Component {...pageProps} />
+    </FirebaseContext.Provider>
+  )
 }
 
 export default MyApp
