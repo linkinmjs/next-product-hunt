d4776a569686d04c4b3def9d8d7bbf997bee7bc5
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-03 12:07:26 -0300

    Creando los archivos de configuración de firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBvMteaYJNPOm7z5dOOKCaN9-TZVew8GXM",
+    authDomain: "product-hunt-2336c.firebaseapp.com",
+    projectId: "product-hunt-2336c",
+    storageBucket: "product-hunt-2336c.appspot.com",
+    messagingSenderId: "833588913335",
+    appId: "1:833588913335:web:10f4c33d716425786c5932",
+    measurementId: "G-BN2XD50FSR"
+};
+
+export default firebaseConfig;
