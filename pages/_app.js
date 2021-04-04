a1e52705130ec0f8090087577b9aa8e79441ba6b
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-04 20:40:08 -0300

    Guardar el usuario autenticado, incluso al recargar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
 import firebase, { FirebaseContext, firebaseContext } from '../firebase';
+import useAutenticacion from '../hooks/useAutenticacion';
 
 function MyApp({ Component, pageProps }) {
 
-  
+  const usuario = useAutenticacion();
+  console.log(usuario);
 
   return (
     <FirebaseContext.Provider
