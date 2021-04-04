9a08b3d46d09936609127b6f7813d8526b5346ae
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-04 20:28:10 -0300

    Iniciando sesión de usuarios

--- pages/login.js
+++ pages/login.js
@@ -24,8 +24,14 @@ export default function Login() {
 
   const { email, password } = valores;
 
-  function iniciarSesion(){
-    console.log('iniciando sesión')
+  async function iniciarSesion(){
+    try {
+      await firebase.login(email, password)
+      Router.push('/');
+    } catch (error) {
+      console.error('Hubo un error al autenticar el usuario ', error.message);
+      guardarError(error.message)
+    }
   }
 
   return (
