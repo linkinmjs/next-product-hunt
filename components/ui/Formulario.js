e21628beb9d7beb3ac87ab7878d6b14f826ec8ea
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-06 18:46:15 -0300

    Creando el Formulario

--- components/ui/Formulario.js
+++ components/ui/Formulario.js
@@ -4,6 +4,13 @@ export const Formulario = styled.form`
     max-width: 600px;
     width: 95%;
     margin: 5rem auto 0 auto;
+
+    fieldset {
+        margin: 2rem 0;
+        border: 1px solid #e1e1e1;
+        font-size: 2rem;
+        padding: 2rem;
+    }
 `;
 
 export const Campo = styled.div`
@@ -19,6 +26,14 @@ export const Campo = styled.div`
     input {
         flex: 1;
         padding: 1rem;
+    }
+
+    input, textarea {
+        flex: 1;
+        padding: 1rem;
+    }
+    textarea {
+        height: 400px;
     }
 `;
 
