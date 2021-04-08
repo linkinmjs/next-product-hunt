ffb81e03b07abfc4232ba1bab2e3be744176494f
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-08 14:44:16 -0300

    Validar que un producto exista

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -1,20 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useRouter } from 'next/router';
+import Error404 from '../../components/layout/404';
+import Layout from '../../components/layout/Layout';
+import { FirebaseContext } from '../../firebase';
 
 const Producto = () => {
-    
+
+    // state del componente
+    const [producto, guardarProducto] = useState({});
+    const [error, guardarError] = useState(false);
+
     // Routing para obtener el id actual
     const router = useRouter();
-    const { query: {id} } = router;
+    const { query: { id } } = router;
+    //console.log(id);
 
-    useEffect(()=>{
-        if(id){
-            console.log('ya existe un id', id);
+    // context de firebase
+    const { firebase } = useContext(FirebaseContext);
+
+    useEffect(() => {
+        if (id) {
+            const obtenerProducto = async () => {
+                const productoQuery = await firebase.db.collection('productos').doc(id);
+                const producto = await productoQuery.get();
+                if (producto.exists) {
+                    guardarProducto(producto.data());
+                } else {
+                    guardarError(true);
+                }
+            }
+            obtenerProducto();
         }
-    }, [id])
+    }, [id]);
+
+
 
     return (
-        <h1>desde {id}</h1>
+        <Layout>
+            {error && <Error404 />}
+        </Layout>
     );
 }
 
