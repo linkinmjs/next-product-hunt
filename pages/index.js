2daa758ea1c109e99f4830e9bbdaf225498ab6cc
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-08 10:54:07 -0300

    Creando el Componente de Producto

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react';
 import Layout from '../components/layout/Layout'
 import {FirebaseContext} from '../firebase';
+import DetallesProducto from '../components/layout/DetallesProducto';
 
 export default function Home() {
 
@@ -22,14 +23,25 @@ export default function Home() {
         ...doc.data()
       }
     })
-
+    // console.log(productos)
     guardarProductos(productos);
   }
 
   return (
     <div>
       <Layout>
-        <h1>Inicio</h1>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {productos.map(producto =>(
+                <DetallesProducto
+                  key={producto.id}
+                  producto={producto}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   )
