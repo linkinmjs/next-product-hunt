17ffb54169e685f7aa5046a400f2d6e46aad667e
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-09 12:22:18 -0300

    Trabajando con los Votos

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -53,6 +53,26 @@ const Producto = () => {
 
     const { comentarios, creado, descripcion, empresa, nombre, url, urlimagen, votos, creador } = producto;
 
+    // administrar y validar los votos
+    const votarProducto = () => {
+        if(!usuario){
+            return router.push('/login');
+        }
+        
+        // obtener y sumar un nuevo voto
+        const nuevoTotal = votos + 1
+
+        // actualizar en la DB
+        firebase.db.collection('productos').doc(id).update({votos: nuevoTotal});
+
+        // actualizar el state
+        guardarProducto({
+            ...producto,
+            votos: nuevoTotal
+        })
+
+    }
+
     return (
         <Layout>
 
@@ -120,7 +140,9 @@ const Producto = () => {
                         </div>
 
                         {usuario && (
-                            <Boton>Votar</Boton>
+                            <Boton
+                                onClick={votarProducto}
+                            >Votar</Boton>
                         )}
                     </aside>
                 </ContenedorProducto>
