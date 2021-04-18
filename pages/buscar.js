73c9aa46803be1ff35955489b228fd11d1805d78
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-18 10:48:34 -0300

    Pasando la búsqueda al componente

--- pages/buscar.js
+++ pages/buscar.js
@@ -1,11 +1,20 @@
 import Layout from '../components/layout/Layout'
+import { useRouter } from 'next/router';
 
-const Buscar = () => (
+const Buscar = () => {
+
+  const router = useRouter();
+  const {query: {q}} = router;
+
+  console.log(q);
+
+  return (
     <div>
       <Layout>
         <h1>Buscar</h1>
       </Layout>
     </div>
   )
+}
 
-  export default Buscar
+export default Buscar
